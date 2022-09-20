458ad1e65a406fee3d5fea4641add6f2ba44b3c4
eeshurtliff <eeshurtliff@gmail.com>
2022-09-20 16:04:44 -0600

    Completed Lesson 1 tasks

---
+++ Programming building blocks/favoritecolor.py
@@ -0,0 +1,7 @@
+favorite_color = input('Please type your favorite color: ')
+print('Your favorite color is: ' , favorite_color)
+
+object = input('What is your favorite object that is that color? ' )
+print('Your favorite ' , favorite_color, 'object is a ' , object)
+
+
